f8e297b6bdb83fa34e33cb1726e90cf57a12769c
marjunstudio <uebujie@gmail.com>
2022-04-05 21:07:25 +0900

    4-8 end

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,10 @@ class Task < ApplicationRecord
     validates :name, presence: true, length: { maximum: 30 }
     validate :validate_name_not_including_commma
 
+    belongs_to :user
+
+    scope :recent, -> {  order(crated_at: :desc) }
+
     private
 
     def validate_name_not_including_commma
